8492a117abc222aec6328f7f52cfe4a7e46f2d62
erwynkarl5 <erwynkarl5@gmail.com>
2026-07-17 22:38:51 +0800

    Create figmaPractice.js

---
+++ figmaPractice.js
@@ -0,0 +1,45 @@
+// const birthYear = 1999
+// let currentYear = 2025  
+
+// const age = currentYear - birthYear 
+
+// console.log(age)
+
+
+// const adjective = 'fluffy'
+// const noun = 'cat'
+// const verb = 'jumped'
+
+// const sentence = 'The' + ' ' + adjective +' ' +  noun + ' ' + verb + ' quickly.'
+
+// console.log(sentence)
+
+// function getGrade(score) {
+//   // Use if / else if / else
+
+// const A = 'A'
+// const B = 'B'
+// const C = 'C'
+// const D = 'D'
+// const F = 'F'
+
+// if (score >= 90) {
+// return A
+// } else if (score >= 80) {
+// return B
+// } else if (score >= 70) {
+// return C
+// } else if (score >= 60) {
+// return D
+// } else {
+// return F
+// }
+//  }
+
+
+
+// console.log(getGrade(95))  // A
+// console.log(getGrade(82))  // B
+// console.log(getGrade(71))  // C
+// console.log(getGrade(55)) 
+
